59bacfc52581fcef616fa8e3ad2a6c6e5fb4fc20
Muhammad Mahmudun Nabi <mmnabi.cse@gmail.com>
2022-06-21 20:40:39 +0600

    Added Api Project, configured and added categories controller

---
+++ MuhammadNabi.CleanArchitectureDemo.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MuhammadNabi.CleanArchitectureDemo.Application.Features.Categories.Commands.CreateCategory;
+using MuhammadNabi.CleanArchitectureDemo.Application.Features.Categories.Queries.GetCategoryList;
+using MuhammadNabi.CleanArchitectureDemo.Application.Features.Categories.Queries.GetCategoryListWithEvents;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MuhammadNabi.CleanArchitectureDemo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CategoryController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("all", Name = "GetAllCategories")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<CategoryListViewModel>>> GetAllCategories()
+        {
+            var dtos = await _mediator.Send(new GetCategoryListQuery());
+            return Ok(dtos);
+        }
+
+        [HttpGet("allwithevents", Name = "GetCategoriesWithEvents")]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<CategoryEventListViewModel>>> GetCategoriesWithEvents(bool includeHistory)
+        {
+            var dtos = await _mediator.Send(new GetCategoryListWithEventsQuery() { IncludeHistory = includeHistory });
+            return Ok(dtos);
+        }
+
+        [HttpPost(Name = "AddCategory")]
+        public async Task<ActionResult<CreateCategoryCommandResponse>> Create([FromBody] CreateCategoryCommand createCategoryCommand)
+        {
+            var response = await _mediator.Send(createCategoryCommand);
+            return Ok(response);
+        }
+    }
+}
--- MuhammadNabi.CleanArchitectureDemo.Application/Profiles/MappingProfile.cs
+++ MuhammadNabi.CleanArchitectureDemo.Application/Profiles/MappingProfile.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using MuhammadNabi.CleanArchitectureDemo.Application.Features.Categories.Commands.CreateCategory;
 using MuhammadNabi.CleanArchitectureDemo.Application.Features.Categories.Queries.GetCategoryList;
 using MuhammadNabi.CleanArchitectureDemo.Application.Features.Categories.Queries.GetCategoryListWithEvents;
+using MuhammadNabi.CleanArchitectureDemo.Application.Features.Events.Commands.CreateEvent;
+using MuhammadNabi.CleanArchitectureDemo.Application.Features.Events.Commands.UpdateEvent;
 using MuhammadNabi.CleanArchitectureDemo.Application.Features.Events.Queries.GetEventDetail;
 using MuhammadNabi.CleanArchitectureDemo.Application.Features.Events.Queries.GetEventList;
 using MuhammadNabi.CleanArchitectureDemo.Domain.Entities;
@@ -12,16 +15,16 @@ namespace MuhammadNabi.CleanArchitectureDemo.Application.Profiles
         public MappingProfile()
         {
             CreateMap<Event, EventListViewModel>().ReverseMap();
-            //CreateMap<Event, CreateEventCommand>().ReverseMap();
-            //CreateMap<Event, UpdateEventCommand>().ReverseMap();
+            CreateMap<Event, CreateEventCommand>().ReverseMap();
+            CreateMap<Event, UpdateEventCommand>().ReverseMap();
             CreateMap<Event, EventDetailViewModel>().ReverseMap();
-            //CreateMap<Event, CategoryEventDto>().ReverseMap();
+            CreateMap<Event, CategoryEventDto>().ReverseMap();
 
             CreateMap<Category, CategoryDto>();
             CreateMap<Category, CategoryListViewModel>();
             CreateMap<Category, CategoryEventListViewModel>();
-            //CreateMap<Category, CreateCategoryCommand>();
-            //CreateMap<Category, CreateCategoryDto>();
+            CreateMap<Category, CreateCategoryCommand>();
+            CreateMap<Category, CreateCategoryDto>();
 
             //CreateMap<Order, OrdersForMonthDto>();
         }
