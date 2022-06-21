2437be9c0ef809c7ea7b335bf878c66a5449c42a
Muhammad Mahmudun Nabi <mmnabi.cse@gmail.com>
2022-06-21 21:51:26 +0600

    Unit test project for Application added.

---
+++ MuhammadNabi.CleanArchitectureDemo.Application.UnitTests/Categories/Commands/CreateCategoryTests.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Moq;
+using MuhammadNabi.CleanArchitectureDemo.Application.Contracts.Persistence;
+using MuhammadNabi.CleanArchitectureDemo.Application.Features.Categories.Commands.CreateCategory;
+using MuhammadNabi.CleanArchitectureDemo.Application.Profiles;
+using MuhammadNabi.CleanArchitectureDemo.Application.UnitTests.Mocks;
+using MuhammadNabi.CleanArchitectureDemo.Domain.Entities;
+using Shouldly;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MuhammadNabi.CleanArchitectureDemo.Application.UnitTests.Categories.Commands
+{
+    public class CreateCategoryTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IAsyncRepository<Category>> _mockCategoryRepository;
+
+        public CreateCategoryTests()
+        {
+            _mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = configurationProvider.CreateMapper();
+        }
+
+        [Fact]
+        public async Task Handle_ValidCategory_AddedToCategoriesRepo()
+        {
+            var handler = new CreateCategoryCommandHandler(_mapper, _mockCategoryRepository.Object);
+
+            await handler.Handle(new CreateCategoryCommand() { Name = "Test" }, CancellationToken.None);
+
+            var allCategories = await _mockCategoryRepository.Object.ListAllAsync();
+            allCategories.Count.ShouldBe(5);
+        }
+    }
+}
---
+++ MuhammadNabi.CleanArchitectureDemo.Application.UnitTests/Categories/Queries/GetCategoriesListQueryHandlerTests.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Moq;
+using MuhammadNabi.CleanArchitectureDemo.Application.Contracts.Persistence;
+using MuhammadNabi.CleanArchitectureDemo.Application.Features.Categories.Queries.GetCategoryList;
+using MuhammadNabi.CleanArchitectureDemo.Application.Profiles;
+using MuhammadNabi.CleanArchitectureDemo.Application.UnitTests.Mocks;
+using MuhammadNabi.CleanArchitectureDemo.Domain.Entities;
+using Shouldly;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MuhammadNabi.CleanArchitectureDemo.Application.UnitTests.Categories.Queries
+{
+    public class GetCategoriesListQueryHandlerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IAsyncRepository<Category>> _mockCategoryRepository;
+
+        public GetCategoriesListQueryHandlerTests()
+        {
+            _mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = configurationProvider.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetCategoriesListTest()
+        {
+            var handler = new GetCategoryListQueryHandler(_mapper, _mockCategoryRepository.Object);
+
+            var result = await handler.Handle(new GetCategoryListQuery(), CancellationToken.None);
+
+            result.ShouldBeOfType<List<CategoryListViewModel>>();
+
+            result.Count.ShouldBe(4);
+        }
+    }
+}
