08eff8a6a863b8d46f6ccf99315ec45cc5fc2b0a
Muhammad Mahmudun Nabi <mmnabi.cse@gmail.com>
2022-06-21 21:26:39 +0600

    Added swagger

--- MuhammadNabi.CleanArchitectureDemo.Api/Startup.cs
+++ MuhammadNabi.CleanArchitectureDemo.Api/Startup.cs
@@ -1,18 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using MuhammadNabi.CleanArchitectureDemo.Api.Utility;
 using MuhammadNabi.CleanArchitectureDemo.Application;
 using MuhammadNabi.CleanArchitectureDemo.Infrastructure;
 using MuhammadNabi.CleanArchitectureDemo.Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MuhammadNabi.CleanArchitectureDemo.Api
 {
@@ -28,6 +23,8 @@ namespace MuhammadNabi.CleanArchitectureDemo.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            AddSwagger(services);
+
             services.AddApplicationServices();
             services.AddInfrastructureServices(Configuration);
             services.AddPersistenceServices(Configuration);
@@ -36,6 +33,21 @@ namespace MuhammadNabi.CleanArchitectureDemo.Api
             services.AddCors(options =>
             {
                 options.AddPolicy("Open", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            });
+        }
+
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "GloboTicket Ticket Management API",
+
+                });
+
+                c.OperationFilter<FileResultContentTypeOperationFilter>();
             });
         }
 
@@ -50,6 +62,12 @@ namespace MuhammadNabi.CleanArchitectureDemo.Api
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "GloboTicket Ticket Management API");
+            });
 
             app.UseCors("Open");
 
