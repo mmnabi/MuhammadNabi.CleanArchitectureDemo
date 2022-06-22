8e6ef856c49262f381494c28d16b2279f1629f33
Muhammad Mahmudun Nabi <mmnabi.cse@gmail.com>
2022-06-22 09:57:30 +0600

    API Integration test project added

---
+++ MuhammadNabi.CleanArchitectureDemo.API.IntegrationTests/Base/CustomWebApplicationFactory.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MuhammadNabi.CleanArchitectureDemo.Persistence;
+using System;
+using System.Net.Http;
+
+namespace MuhammadNabi.CleanArchitectureDemo.API.IntegrationTests.Base
+{
+    public class CustomWebApplicationFactory<TStartup>
+            : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+
+                services.AddDbContext<GloboTicketDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("GloboTicketDbContextInMemoryTest");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var context = scopedServices.GetRequiredService<GloboTicketDbContext>();
+                    var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    context.Database.EnsureCreated();
+
+                    try
+                    {
+                        Utilities.InitializeDbForTests(context);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, $"An error occurred seeding the database with test messages. Error: {ex.Message}");
+                    }
+                };
+            });
+        }
+
+        public HttpClient GetAnonymousClient()
+        {
+            return CreateClient();
+        }
+    }
+}
---
+++ MuhammadNabi.CleanArchitectureDemo.API.IntegrationTests/Controllers/CategoryControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using MuhammadNabi.CleanArchitectureDemo.Api;
+using MuhammadNabi.CleanArchitectureDemo.API.IntegrationTests.Base;
+using MuhammadNabi.CleanArchitectureDemo.Application.Features.Categories.Queries.GetCategoryList;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MuhammadNabi.CleanArchitectureDemo.API.IntegrationTests.Controllers
+{
+    public class CategoryControllerTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public CategoryControllerTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task ReturnsSuccessResult()
+        {
+            var client = _factory.GetAnonymousClient();
+
+            var response = await client.GetAsync("/api/category/all");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            var result = JsonConvert.DeserializeObject<List<CategoryListViewModel>>(responseString);
+
+            Assert.IsType<List<CategoryListViewModel>>(result);
+            Assert.NotEmpty(result);
+        }
+    }
+}
